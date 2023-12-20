fe5fa1b279c5cb5aad4f8696113d2f00408230f3
maann7 <luthercivil019@gmail.com>
2023-12-20 15:24:04 +0530

    logout button added

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import os
 import pymysql
 
 app = Flask(__name__)
-<<<<<<< HEAD
-conn = pymysql.connect(host="localhost", user="root", password="Sharmi@2020", db="TalkWave")
-=======
-conn = pymysql.connect(host="localhost", user="root", password="root", db="TalkWave")
->>>>>>> ce0ba4c4496ac572fa0eb30bb91e173f071da10d
+conn = pymysql.connect(host="localhost", user="root", password="Luther@1234", db="TalkWave")
 cursor = conn.cursor()
 
 app_root = os.path.dirname(os.path.abspath(__file__))
@@ -18,20 +14,18 @@ app_root = app_root+"/static"
 def logo_animate():
     return render_template("logo_animate.html")
 
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
 
 @app.route("/navbar")
 def navbar():
     return render_template("navbar.html")
 
 
-<<<<<<< HEAD
-=======
-@app.route("/")
-def login():
-    return render_template("login.html")
+
 
 
 @app.route("/account_registration")
@@ -60,6 +54,5 @@ def account_login():
     else:
         return render_template("msg.html", message="Please Enter Valid Credentials")
 
->>>>>>> ce0ba4c4496ac572fa0eb30bb91e173f071da10d
 
 app.run(debug=True)
