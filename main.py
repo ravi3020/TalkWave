a31aa5c589966782c305da2237e3351faad66837
JhontyTarun <grandhikumar108@gmail.com>
2023-12-20 13:04:02 +0530

    Account Registration

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import pymysql
 
 app = Flask(__name__)
-conn = pymysql.connect(host="localhost", user="root", password="Luther@1234", db="TalkWave")
+conn = pymysql.connect(host="localhost", user="root", password="root", db="TalkWave")
 cursor = conn.cursor()
 
 app_root = os.path.dirname(os.path.abspath(__file__))
@@ -18,6 +18,22 @@ def navbar():
 @app.route("/")
 def login():
     return render_template("login.html")
+
+
+@app.route("/account_registration")
+def account_registration():
+    account_name = request.args.get("account_name")
+    account_username = request.args.get("account_username")
+    account_email = request.args.get("account_email")
+    account_password = request.args.get("account_password")
+    gender = request.args.get("gender")
+    count = cursor.execute("select * from account where account_username = '"+str(account_username)+"' or account_email = '"+str(account_email)+"'")
+    if count == 0:
+        cursor.execute("insert into account(account_name, account_username, account_email, account_password, gender) value('"+str(account_name)+"', '"+str(account_username)+"', '"+str(account_email)+"', '"+str(account_password)+"', '"+str(gender)+"')")
+        conn.commit()
+        return redirect("/")
+    else:
+        return render_template("msg.html", message="Already Exist")
 
 
 
