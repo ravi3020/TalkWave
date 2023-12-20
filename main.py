228f3b2621dbd3c270be61355b3166349e79c99d
sharmila1922 <sksharmila1922@gmail.com>
2023-12-20 13:38:50 +0530

    logo

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ import os
 import pymysql
 
 app = Flask(__name__)
-conn = pymysql.connect(host="localhost", user="root", password="Luther@1234", db="TalkWave")
+conn = pymysql.connect(host="localhost", user="root", password="Sharmi@2020", db="TalkWave")
 cursor = conn.cursor()
 
 app_root = os.path.dirname(os.path.abspath(__file__))
 app_root = app_root+"/static"
 
 
+@app.route("/")
+def logo_animate():
+    return render_template("logo_animate.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/navbar")
 def navbar():
     return render_template("navbar.html")
-
-
-@app.route("/")
-def login():
-    return render_template("login.html")
 
 
 
